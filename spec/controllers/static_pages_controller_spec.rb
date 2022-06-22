7c06301131105df0202b7efd66de0a7cb3527cf1
gustave-studio <gustave0222@gmail.com>
2022-06-23 07:52:45 +0900

    contactページ追加

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -9,21 +9,29 @@ RSpec.describe StaticPagesController, type: :controller do
       get :home
       expect(response).to have_http_status(200)
       expect(response.body).to include('Ruby on Rails Tutorial Sample App')
-      expect(response.body).to have_title('Home | Ruby on Rails Tutorial Sample App')
+      expect(response.body).to have_title('Ruby on Rails Tutorial Sample App')
     end
 
-    it 'homeが表示される' do
+    it 'helpが表示される' do
       get :help
       expect(response).to have_http_status(200)
       expect(response.body).to include("Help | Ruby on Rails Tutorial Sample App")
       expect(response.body).to have_title('Help | Ruby on Rails Tutorial Sample App')
     end
 
-    it 'homeが表示される' do
+    it 'aboutが表示される' do
       get :about
       expect(response).to have_http_status(200)
       expect(response.body).to include("About | Ruby on Rails Tutorial Sample App")
       expect(response.body).to have_title('About | Ruby on Rails Tutorial Sample App')
     end
+
+    it 'contactが表示される' do
+      get :contact
+      expect(response).to have_http_status(200)
+      expect(response.body).to include("Contact | Ruby on Rails Tutorial Sample App")
+      expect(response.body).to have_title('Contact | Ruby on Rails Tutorial Sample App')
+    end
+
   end
 end
